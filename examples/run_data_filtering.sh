a18b035c384a5224b035a91c402d4298cd7119a8
jacksonpradolima <pradolima@live.com>
2021-04-28 11:54:42 -0300

    removed old examples

--- examples/run_data_filtering.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-
-python3 data_filtering.py -r "data/deeplearning4j@deeplearning4j"
--- examples/run_project_status.sh
+++
@@ -1,4 +0,0 @@
-cd ..
-
-python3 project_status.py --dataset_dir "data" --datasets 'deeplearning4j@deeplearning4j' --output_dir 'results/dataset_info/'
-python3 project_status.py --is_visualization 'true' --dataset_dir "data" --datasets 'deeplearning4j@deeplearning4j' --output_dir 'results/dataset_info/'
